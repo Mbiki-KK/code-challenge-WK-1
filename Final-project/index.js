65694b140407a7ca0f8619639a757adc89590159
Mbiki-KK <wawerumbiki@gmail.com>
2023-04-05 12:10:09 +0300

    html,css,javascript, done

---
+++ Final-project/index.js
@@ -0,0 +1,22 @@
+const photoArea = document.getElementById("Photo-area");
+const generateBtn = document.getElementById("photo-gen");
+
+generateBtn.addEventListener("click", () => {
+  fetch("https://picsum.photos/200/300")
+    .then((response) => {
+      photoArea.innerHTML = `<img src="${response.url}" alt="Generated Photo">`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+const likeBtn = document.getElementById("like-btn");
+const likeCount = document.getElementById("like-count");
+let likes = 0;
+
+likeBtn.addEventListener("click", () => {
+  likes++;
+  likeCount.textContent = `${likes} ${likes === 1 ? 'like' : 'likes'}`;
+});
+
